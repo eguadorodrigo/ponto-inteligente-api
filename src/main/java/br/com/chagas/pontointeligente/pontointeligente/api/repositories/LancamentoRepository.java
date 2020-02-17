17416a991ec542d9b1dcc9e0326dea59cc877cc0
Rodrigo <rrodrigocmiranda@gmail.com>
2020-02-17 13:25:51 -0300

    Atividade do dia.

--- src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/repositories/LancamentoRepository.java
@@ -1,5 +1,27 @@
 package br.com.chagas.pontointeligente.pontointeligente.api.repositories;
 
-public interface LancamentoRepository {
+
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.chagas.pontointeligente.pontointeligente.api.model.Lancamento;
+
+@Transactional(readOnly = true)
+@NamedQueries({
+	@NamedQuery(name="LancamentoRepository.findByFuncionarioId", 
+				query="SELECT lanc FROM lancamento lanc WHERE lanc.funcionario.id = :funcionarioId")})
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+	
+	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+	
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
     
 }
