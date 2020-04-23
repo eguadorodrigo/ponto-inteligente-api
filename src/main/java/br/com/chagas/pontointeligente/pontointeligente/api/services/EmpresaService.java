9ec05243f2837f0591580a364c4e160125c953a4
Rodrigo C. de Miranda <rrodrigocmiranda@gmail.com>
2020-04-23 00:41:42 -0300

    Criacao do service de Empresa e sua implementação.

---
+++ src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,23 @@
+package br.com.chagas.pontointeligente.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import br.com.chagas.pontointeligente.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaService {
+    /**
+     * Retorna uma empresa dado um CNPJ.
+     * 
+     * @param cnpj
+     * @return Optional<Empresa>
+     */
+    Optional<Empresa> buscarPorCnpj(String cnpj);
+
+    /**
+     * Cadastra uma nova empresa na base de dados.
+     * 
+     * @param empresa
+     * @return Empresa
+     */
+    Empresa persistir(Empresa empresa);
+}
---
+++ src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/services/EmpresaServiceImpl.java
@@ -0,0 +1,34 @@
+package br.com.chagas.pontointeligente.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.chagas.pontointeligente.pontointeligente.api.entities.Empresa;
+import br.com.chagas.pontointeligente.pontointeligente.api.repositories.EmpresaRepository;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+    public static final Logger logger =  LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+
+    @Override
+    public Optional<Empresa> buscarPorCnpj(String cnpj) {
+        logger.info("Buscando uma empresa por CNPJ{}", cnpj);
+        return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+    }
+
+    @Override
+    public Empresa persistir(Empresa empresa) {
+        logger.info("Persistindo empresa: {}", empresa);
+        return this.empresaRepository.save(empresa);
+    }
+    
+}
