61d2435760f475466d5bd67a0388700baa66600a
Rodrigo C. de Miranda <rrodrigocmiranda@gmail.com>
2020-04-26 19:01:24 -0300

    Criação da classe LancamentoService

---
+++ src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/services/LancamentoService.java
@@ -0,0 +1,41 @@
+package br.com.chagas.pontointeligente.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import br.com.chagas.pontointeligente.pontointeligente.api.entities.Lancamento;
+
+public interface LancamentoService {
+
+    /**
+     * Retorna uma lista paginada de lancamentos de um determinado funcionario.
+     * @param funcionarioId
+     * @param pageRequest
+     * @return Page<Lancamento>
+     */
+    Page<Lancamento> buscarFuncionarioId(Long funcionarioId, PageRequest pageRequest);
+
+
+    /**
+     * Retorna um lancamento por ID.
+     * @param id
+     * @return Optional<Lancamento>
+     */
+    Optional<Lancamento> buscarPorId(Long id);
+
+    /**
+     * Persiste um novo objeto lancamento na base de dados.
+     * @param lancamento
+     * @return lancamento
+     */
+    Lancamento persistir(Lancamento lancamento);
+
+    /**
+     * Remove um lancamento da base de dados.
+     * @param id
+     */
+    void remover(Long id);
+
+}
