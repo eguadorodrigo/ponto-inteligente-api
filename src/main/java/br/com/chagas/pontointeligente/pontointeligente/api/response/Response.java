24b4821d8d62d6b52f62054b9a41ad6a943ea54b
Rodrigo C. de Miranda <rrodrigocmiranda@gmail.com>
2020-04-27 07:58:04 -0300

    getErros() melhorado par não estourar nullpointer

--- src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/response/Response.java
+++ src/main/java/br/com/chagas/pontointeligente/pontointeligente/api/response/Response.java
@@ -1,5 +1,6 @@
 package br.com.chagas.pontointeligente.pontointeligente.api.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -11,5 +12,14 @@ public class Response<T> {
     
     private List<String> erros;
 
-
+    /**
+     * Conversão incluída somente para controle da lista caso seja nula
+     * @return List<String>
+     */
+    public List<String> getErros(){
+        if (this.erros == null) {
+            this.erros = new ArrayList<String>();            
+        }
+        return erros;
+    } 
 }
